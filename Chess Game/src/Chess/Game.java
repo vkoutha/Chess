5730882acc95ddb777bcc42cf95dc537a4f015f3
vkout <vkout@DESKTOP-TQH1ORJ.home>
2018-11-25 17:40:32 -0500

    Adjusted tile hit boxes

--- Chess Game/src/Chess/Game.java
+++ Chess Game/src/Chess/Game.java
@@ -690,7 +690,7 @@ public class Game implements ActionListener, MouseListener, KeyListener{
 				
 				if(!inPromotionMenu)
 					if (GameData.singlePlayer || playerTurn == GameData.player.PLAYER_1)
-						tileClicked = new int[]{(int)(Math.floor((e.getY()-GameData.HEIGHT_COMPENSATOR+5)/GameData.TILE_HEIGHT)), (int)Math.floor((e.getX()-(GameData.WIDTH_COMPENSATOR-2))/GameData.TILE_WIDTH)};
+						tileClicked = new int[]{(int)(Math.floor((e.getY()-GameData.HEIGHT_COMPENSATOR-10)/GameData.TILE_HEIGHT)), (int)Math.floor((e.getX()-(GameData.WIDTH_COMPENSATOR+2))/GameData.TILE_WIDTH)};
 					else if (!GameData.singlePlayer && playerTurn == GameData.player.PLAYER_2) 
 						tileClicked = new int[]{(int) (GameData.ROWS-1-(Math.floor((e.getY()-GameData.HEIGHT_COMPENSATOR+5)/GameData.TILE_WIDTH))), (int)(GameData.ROWS-1-(Math.floor((e.getX()-(GameData.WIDTH_COMPENSATOR-2))/GameData.TILE_HEIGHT)))};
 			}
